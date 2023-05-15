08de3918d34bc44778b88ade26927faf45b3a6dc
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-15 15:11:30 +0300

    create add note design

--- app/src/main/java/com/fighter/honeynotes/ui/addNote/AddNoteFragment.kt
+++ app/src/main/java/com/fighter/honeynotes/ui/addNote/AddNoteFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.fighter.honeynotes.databinding.FragmentAddNoteBinding
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : DialogFragment() {
     private lateinit var binding: FragmentAddNoteBinding
 
     override fun onCreateView(
