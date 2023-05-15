ff07f4d4f7d4c8f09d27e907413b61f6bc023398
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-15 15:54:36 +0300

    create bottom sheet fragment

--- app/src/main/java/com/fighter/honeynotes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fighter/honeynotes/ui/home/HomeFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.fighter.honeynotes.databinding.FragmentAddNoteBinding
 import com.fighter.honeynotes.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class HomeFragment : Fragment() {
-
-    private lateinit var binding:FragmentHomeBinding
+    private lateinit var bottomSheet: BottomSheetDialog
+    private lateinit var addNoteBinding: FragmentAddNoteBinding
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +26,17 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fabAddNote.setOnClickListener {
+            initiateBottomSheet()
+        }
 
+    }
+
+    private fun initiateBottomSheet() {
+        bottomSheet = BottomSheetDialog(requireContext())
+        addNoteBinding = FragmentAddNoteBinding.inflate(layoutInflater)
+        bottomSheet.setContentView(addNoteBinding.root)
+        bottomSheet.show()
     }
 
 }
