6794f4c76ecfc5c879e0b3371b0e555f7a91bbb1
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-15 20:20:12 +0300

    add navigation graph

--- app/src/main/java/com/fighter/honeynotes/ui/MainActivity.kt
+++ app/src/main/java/com/fighter/honeynotes/ui/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fighter.honeynotes.R
 import com.fighter.honeynotes.data.NoteDatabase
+import com.fighter.honeynotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        NoteDatabase.getInstance(this) // This is work around instance before study dependency injection
+        NoteDatabase.getInstance(applicationContext) // This is work around instance before study dependency injection
     }
 }
